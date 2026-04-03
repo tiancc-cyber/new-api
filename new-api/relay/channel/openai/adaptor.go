cc0437e1920e0ef192f432b9e3c01491609c1698
田晨晨 <18222006090@163.com>
2026-04-03 13:54:18 +0800

    修改名称为词元视界

--- new-api/relay/channel/openai/adaptor.go
+++ new-api/relay/channel/openai/adaptor.go
@@ -229,7 +229,7 @@ func (a *Adaptor) SetupRequestHeader(c *gin.Context, header *http.Header, info *
 			header.Set("HTTP-Referer", "https://www.newapi.ai")
 		}
 		if header.Get("X-OpenRouter-Title") == "" {
-			header.Set("X-OpenRouter-Title", "New API")
+			header.Set("X-OpenRouter-Title", "词元视界")
 		}
 	}
 	return nil
